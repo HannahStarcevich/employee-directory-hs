b9626635c8f4f2ace46663aa06efbcf687077f3d
HannahStarcevich <starcevichh@gmail.com>
2021-02-17 07:55:34 -0800

    updates to styling

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,25 +1,36 @@
 import React from "react";
+import "./style.css";
+
 
 function SearchBar(props) {
   return (
     <form>
       <div className="form-group">
-        <label htmlFor="search">Search:</label>
-        <input
-          onChange={props.onInputChange}
-          value={props.search}
-          name="search"
-          type="text"
-          className="form-control"
-          placeholder="Search for an Employee"
-          id="search"
-        />
-        <button onClick={props.onFormSubmit} className="btn btn-primary mt-3">
-          Search
-        </button>
-        <button onClick={props.clearFilter} className="btn btn-danger mt-3">
-          Clear
-        </button>
+        <div className="container">
+          <div className="row">
+          <div className="col-4"></div>
+            <div className="col-4 search-input">
+              <label htmlFor="search">Search:</label>
+              <input
+                onChange={props.onInputChange}
+                value={props.search}
+                name="search"
+                type="text"
+                className="form-control"
+                placeholder="Search for an Employee"
+                id="search"
+              />
+            </div>
+            <div className="col-4">
+                <button onClick={props.onFormSubmit} className="btn btn-primary mt-3">
+                  Search
+                </button>
+                <button onClick={props.clearFilter} className="btn btn-outline-primary mt-3">
+                  Clear
+                </button>
+            </div>
+          </div>
+        </div>
       </div>
     </form>
   );
